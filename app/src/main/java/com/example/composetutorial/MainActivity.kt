917f692f9bd0797ce6e8fdce206f4f080a17afbc
Ojongseok <whdtjr6889@naver.com>
2023-07-30 13:34:25 +0900

    [Compose 기본사항] 시작 튜토리얼 - LazyColumn으로 리사이클러뷰처럼 만들기

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,35 +1,37 @@
 package com.example.composetutorial
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
-import android.content.res.Configuration
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.text.style.TextOverflow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,7 +39,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeTutorialTheme {
                 Surface {
-                    MessageCard(msg = Message("Author", "Body"))
+                    Conversation(messages = SampleData.conversationSample)
                 }
             }
         }
@@ -48,56 +50,56 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Row(
-        modifier = Modifier
-            .border(width = 2.dp, color = MaterialTheme.colorScheme.primary, shape = RectangleShape)
-            .padding(all = 12.dp)
-            .background(color = MaterialTheme.colorScheme.inversePrimary, shape = RoundedCornerShape(12.dp))
-            .padding(all = 12.dp)
-    ) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(id = R.drawable.sample_image),
             contentDescription = "sample",
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+                .border(2.dp, MaterialTheme.colorScheme.secondary, CircleShape)
         )
+        Spacer(modifier = Modifier.width(8.dp))
 
-        Spacer(modifier = Modifier.width(12.dp))
+        var isExpended by remember {
+            mutableStateOf(false)
+        }
 
-        Column {
+        Column(modifier = Modifier.clickable { isExpended = !isExpended }) {
             Text(
                 text = msg.author,
-                color = MaterialTheme.colorScheme.secondary,
-                style = MaterialTheme.typography.bodyLarge
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.titleLarge
             )
-
-            Spacer(modifier = Modifier.height(4.dp))
-
-            Surface(shape = RoundedCornerShape(8.dp), shadowElevation = 1.dp) {
+            Spacer(modifier = Modifier.height(8.dp))
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
                 Text(
-                    modifier = Modifier.padding(vertical = 6.dp, horizontal = 12.dp),
+                    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
                     text = msg.body,
-                    color = MaterialTheme.colorScheme.primary,
-                    style = MaterialTheme.typography.bodySmall,
-                    fontSize = 16.sp
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = if (isExpended) Int.MAX_VALUE else 1,
+                    overflow = TextOverflow.Ellipsis
                 )
+                
             }
         }
     }
 }
 
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "night"
-)
 @Composable
-fun previewMessageCard() {
-    ComposeTutorialTheme() {
-        Surface {
-            MessageCard(msg = Message("안드로이드 컴포즈","Android Compose"))
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) {msg ->
+            MessageCard(msg = msg)
         }
+    }
+}
+
+@Preview
+@Composable
+fun previewConversation() {
+    ComposeTutorialTheme {
+        Conversation(messages = SampleData.conversationSample)
     }
 }

ad644845d6bc6d7d3b9ae1d1dbd8c3fcddd3ceaa
Ojongseok <whdtjr6889@naver.com>
2023-09-18 18:09:31 +0900

    LazyRow로 아이템 배치, contentPadding

---
+++ app/src/main/java/com/example/composetutorial/AlignYourBodyData.kt
@@ -0,0 +1,6 @@
+package com.example.composetutorial
+
+data class AlignYourBodyData(
+    val drawable : Int,
+    val text: Int
+)
--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,37 +1,49 @@
 package com.example.composetutorial
 
-import android.content.res.Configuration.UI_MODE_NIGHT_NO
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.clickable
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,112 +55,140 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ComposeTutorialTheme {
-                Myapp(Modifier.fillMaxSize())
+                AlignYourBodyRow()
             }
         }
     }
 }
 
 @Composable
-fun Myapp(modifier: Modifier = Modifier) {
-    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
-
-    if (shouldShowOnboarding) {
-        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
-    } else {
-        Greetings(onclickListener = {shouldShowOnboarding = true})
-    }
-}
-
-@Composable
-private fun Greetings(
-    modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose","123","123","123","123","123","123","123","123"),
-    onclickListener: () -> Unit
-) {
-    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
-        items(items = names) {
-            Greeting(
-                name = it,
-                onclickListener = {}
+fun SearchBar(modifier: Modifier = Modifier) {
+    TextField(
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
+            .padding(all = 8.dp),
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
             )
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedContainerColor = MaterialTheme.colorScheme.onSurface
+        ),
+        placeholder = {
+            Text(stringResource(R.string.placeholder_search))
         }
-    }
+    )
 }
 
 @Composable
-fun OnboardingScreen(
-    modifier: Modifier = Modifier,
-    onContinueClicked : () -> Unit
+fun AlignYourBodyElement(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
+    modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Welcome to the Basics Codelab!")
-        Button(
-            modifier = modifier.padding(vertical = 24.dp),
-            onClick = onContinueClicked
-        ) {
-            Text(
-                text = "계속하기",
-                fontSize = 18.sp,
-                style = MaterialTheme.typography.headlineMedium.copy(
-                    fontWeight = FontWeight.ExtraBold
-                )
-            )
-        }
-        Text(text = "더보기")
+        Image(
+            painter = painterResource(drawable),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+                .size(88.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            modifier = modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+                .background(color = Color.Blue, shape = RoundedCornerShape(8.dp))
+                .padding(all = 4.dp),
+            text = stringResource(text),
+            color = Color.White
+        )
     }
-
 }
 
 @Composable
-private fun Greeting(name: String, onclickListener: () -> Unit) {
-    var expanded by remember { mutableStateOf(false) }
-
-    val extraPadding by animateDpAsState(
-        if (expanded) 48.dp else 0.dp, label = "",
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-
+fun FavoriteCollectionCard(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
+    modifier: Modifier = Modifier
+) {
     Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        shape = RoundedCornerShape(8.dp),
+        color = MaterialTheme.colorScheme.surfaceVariant,
+        modifier = modifier
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello, ", modifier = Modifier.clickable(onClick = onclickListener))
-                Text(
-                    modifier = Modifier.clickable(onClick = onclickListener),
-                    text = name
-                )
-            }
-            ElevatedButton(
-                onClick = { expanded = !expanded }
-            ) {
-                Text(if (expanded) "Show less" else "Show more")
-            }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.width(255.dp)
+        ) {
+            Image(
+                painter = painterResource(drawable),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(80.dp)
+            )
+            Text(
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(horizontal = 16.dp),
+                text = stringResource(text),
+                fontSize = 12.sp
+            )
         }
     }
 }
 
-@Preview(
-    showBackground = true,
-    uiMode = UI_MODE_NIGHT_YES,
-    name = "Dark"
-)
-@Preview(showBackground = true)
+@Composable
+fun AlignYourBodyRow(
+    modifier: Modifier = Modifier
+) {
+    val alignYourBodyData = mutableListOf<AlignYourBodyData>()
+    alignYourBodyData.add(AlignYourBodyData(R.drawable.sameple_image2, R.string.image_string))
+    alignYourBodyData.add(AlignYourBodyData(R.drawable.sameple_image2, R.string.image_string))
+    alignYourBodyData.add(AlignYourBodyData(R.drawable.sample_image, R.string.image_string))
+    alignYourBodyData.add(AlignYourBodyData(R.drawable.sameple_image2, R.string.image_string))
+    alignYourBodyData.add(AlignYourBodyData(R.drawable.sample_image, R.string.image_string))
+
+    LazyRow(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 4.dp)
+    ) {
+        items(alignYourBodyData) {
+            AlignYourBodyElement(it.drawable, it.text)
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
 @Composable
 fun OnboardingPreview() {
     ComposeTutorialTheme {
-        OnboardingScreen(onContinueClicked = {})
+        AlignYourBodyRow()
     }
 }
+
+//@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+//@Composable
+//fun OnboardingPreview2() {
+//    ComposeTutorialTheme {
+//        SearchBar()
+//    }
+//}
+//
+//@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+//@Composable
+//fun OnboardingPreview3() {
+//    ComposeTutorialTheme {
+//        AlignYourBodyElement()
+//    }
+//}

2b73bb0977222125ef554f4d6ab241c95fa2d6b1
Ojongseok <whdtjr6889@naver.com>
2023-09-19 16:54:43 +0900

    Compose의 이벤트 - 상태 변경 전

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -86,3 +86,4 @@ fun OnboardingPreview() {
         WellnessScreen()
     }
 }
+
--- app/src/main/java/com/example/composetutorial/WaterCounter.kt
+++ app/src/main/java/com/example/composetutorial/WaterCounter.kt
@@ -1,6 +1,8 @@
 package com.example.composetutorial
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,9 +13,16 @@ import androidx.compose.ui.unit.dp
 fun WaterCounter(
     modifier: Modifier = Modifier
 ) {
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
+    Column(
         modifier = modifier.padding(16.dp)
-    )
+    ) {
+        var count = 0
+        Text(text = "You've had $count glasses.")
+        Button(
+            onClick = { count++ },
+            modifier = modifier.padding(top = 8.dp)
+        ) {
+            Text(text = "Add one")
+        }
+    }
 }

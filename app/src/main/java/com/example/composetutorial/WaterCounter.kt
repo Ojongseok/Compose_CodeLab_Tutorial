84551267cd75a5f73bab246929c207092454cb58
Ojongseok <whdtjr6889@naver.com>
2023-09-19 22:40:59 +0900

    Compose의 이벤트 - 상태 변경 remember, remebmberSaveable

--- app/src/main/java/com/example/composetutorial/WaterCounter.kt
+++ app/src/main/java/com/example/composetutorial/WaterCounter.kt
@@ -1,28 +1,67 @@
 package com.example.composetutorial
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(
-    modifier: Modifier = Modifier
-) {
-    Column(
-        modifier = modifier.padding(16.dp)
-    ) {
-        var count = 0
-        Text(text = "You've had $count glasses.")
-        Button(
-            onClick = { count++ },
-            modifier = modifier.padding(top = 8.dp)
-        ) {
-            Text(text = "Add one")
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by rememberSaveable { mutableStateOf(0) }
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
     }
 }
+
+//@Composable
+//fun WaterCounter(
+//    modifier: Modifier = Modifier
+//) {
+//    Column(
+//        modifier = modifier.padding(16.dp)
+//    ) {
+//        var count by remember { mutableStateOf(0) }
+//
+//        if (count > 0) {
+//            var showTask by remember { mutableStateOf(true) }
+//
+//            if (showTask) {
+//                WellnessTaskItem(
+//                    taskName = "Have you taken your 15 minute walk today?",
+//                    onClose = { showTask = false }
+//                )
+//            }
+//            Text(text = "You've had $count glasses.")
+//        }
+//
+//        Row(
+//            Modifier.padding(top = 8.dp)
+//        ) {
+//            Button(onClick = { count++ }, enabled = count < 10) {
+//                Text(text = "Add one")
+//            }
+//            Button(
+//                onClick = { count = 0 },
+//                Modifier.padding(start = 8.dp)
+//            ) {
+//                Text(text = "Clear water count")
+//            }
+//        }
+//    }
+//}
---
+++ app/src/main/java/com/example/composetutorial/WellnessTaskItem.kt
@@ -0,0 +1,39 @@
+package com.example.composetutorial
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = taskName,
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp)
+        )
+        IconButton(
+            onClick = onClose
+        ) {
+            Icon(imageVector = Icons.Default.Close, contentDescription =  "Close")
+        }
+    }
+}
+
